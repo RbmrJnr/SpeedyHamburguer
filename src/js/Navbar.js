266cbc9bd580b431d5e19a92556419909912bcb1
Pedro <eupedrohenrique.euzebio@gmail.com>
2023-07-25 13:41:32 -0300

    :lipstick: style: adiciona efeito de smouth na scrollbar

--- src/js/Navbar.js
+++ src/js/Navbar.js
@@ -7,6 +7,10 @@ $(document).ready(function () {
     }
   })
 
+  $(".navbar .menu li a").click(function () {
+    $("html").css("scrollBehavior", "smooth")
+  })
+
   $(".menu-btn").click(function () {
     $(".navbar .menu").toggleClass("active")
     $(".menu-btn .fas.fa-bars").toggleClass("active")
